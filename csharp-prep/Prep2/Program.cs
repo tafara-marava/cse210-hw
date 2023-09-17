99b9ad4e876371e9c61fada661b59fc6fa4be088
Tafara Marava <tafara.marava2@gmail.com>
2023-09-16 20:22:12 -0600

    Determining letter grades

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Please enter your grade for this class: ");
+        string userGrade = Console.ReadLine();
+
+        int grade = int.Parse(userGrade);
+
+        if (grade >= 90)
+        {
+            Console.WriteLine("You scored an A");
+        }
+        else if (grade >= 80)
+        {
+            Console.WriteLine("You scored an B");  
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine("You scored an C");  
+        }
+        else if (grade >= 60)
+        {
+            Console.WriteLine("You scored an D");  
+        }
+        else 
+        {
+            Console.WriteLine("You scored an F"); 
+            Console.WriteLine("This is not the end of the world, you can try again next semester"); 
+        }
     }
 }
