a037e9a3ea11ee75600a778d16503b1996d49158
Tafara Marava <tafara.marava2@gmail.com>
2023-09-28 17:44:08 -0600

    guessing game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,28 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        int magicNumber = 5;
+        int guess = 0;
+
+        while (guess != magicNumber)
+        {
+            Console.WriteLine("What is your guess? ");
+            int userGuess = int.Parse(Console.ReadLine());
+
+            guess = userGuess; // Update the guess with the user's input
+
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Guess lower ");
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Guess higher ");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
